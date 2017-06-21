331796db94dd34aa154c6f3fc9b7c32b736a574f
Phi Ho <phi.ho@axonactive.vn>
2017-06-21 11:29:11 +0700

    new change

--- bacutoca-server/dbService.js
+++ bacutoca-server/dbService.js
@@ -3,41 +3,22 @@ var sqlite3 = require('sqlite3').verbose();
 
 
 var get = function (query, params) {
+    var user = {};
     return new Promise(function (resolve, reject) {
         openDb().then(function (database) {
-            database.get(query, params, function (err, result) {
-                if (err) {
-                    return reject(err);
-                }
+            database.get(query, params, function (err, user) {
                 database.close();
-                resolve(result);
-            });
-        });
-    });
-};
-
-var all = function (query, params) {
-    return new Promise(function (resolve, reject) {
-        openDb().then(function (database) {
-            database.all(query, params, function (err, results) {
-                if (err) {
-                    return reject(err);
-                }
-                database.close();
-                resolve(results);
+                resolve(user);
             });
         });
     });
 };
 
 var run = function (query, params) {
-    console.log("executing query: " + query);
+    var user = {};
     return new Promise(function (resolve, reject) {
         openDb().then(function (database) {
-            database.run(query, params, function (err) {
-                if (err) {
-                    return reject(err);
-                }
+            database.run(query, params, function () {
                 database.close();
                 resolve(this);
             });
@@ -55,6 +36,5 @@ function openDb() {
 
 module.exports = {
     get: get,
-    run: run,
-    all: all
+    run: run
 };
